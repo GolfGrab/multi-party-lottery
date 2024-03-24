f7232beaa1ee9a13fecfc7ef67a77fd236807188
GolfGrab <surapus01@gmail.com>
2024-03-24 18:37:20 +0700

    update CommitReveal contract

--- lottery.sol
+++ lottery.sol
@@ -4,19 +4,21 @@ import "./CommitReveal.sol";
 import "./Ownable.sol";
 
 contract MultiPartyLottery is Ownable, CommitReveal {
-    uint16 public gameStage = 0; // 0: idle, 1: commit, 2: reveal, 3: award , 4: award timeout
+    uint16 private _gameStage = 0; // 0: idle, 1: playing  [commit, reveal, award,  award timeout]
     uint256 public startStageTime = block.timestamp;
-    uint256 public _T1; //commit stage duration
-    uint256 public _T2; //reveal stage duration
-    uint256 public _T3; //award  stage duration
-    uint256 public _N; // maximum number of lottery participants
-    uint256 public num_participants = 0;
+    uint256 public T1; //commit stage duration
+    uint256 public T2; //reveal stage duration
+    uint256 public T3; //award  stage duration
+    uint256 public N; // maximum number of lottery participants
+    uint256 private _num_participants = 0;
+    uint256 private _num_revealed = 0;
     uint16 constant MinimumChoice = 0;
     uint16 constant MaximumChoice = 999;
     uint256 constant LotteryFee = 0.001 ether;
 
     struct Lottery {
         uint16 choice; // 0 - 999
+        bool isCommitted;
         bool isRevealed;
         bool isWithdrawn;
         address addr;
@@ -30,43 +32,76 @@ contract MultiPartyLottery is Ownable, CommitReveal {
         uint256 _revealStageDuration,
         uint256 _awardStageDuration
     ) {
-        _N = _maximumPlayerLimit;
-        _T1 = _commitLotteryStageDuration;
-        _T2 = _revealStageDuration;
-        _T3 = _awardStageDuration;
+        N = _maximumPlayerLimit;
+        T1 = _commitLotteryStageDuration;
+        T2 = _revealStageDuration;
+        T3 = _awardStageDuration;
     }
 
-    function startGame() public onlyOwner {
-        require(
-            gameStage == 0,
-            "MultiPartyLottery::startGame: Game is not idle"
-        );
-        gameStage = 1;
-        startStageTime = block.timestamp;
+    function getGameStage()
+        public
+        view
+        returns (string memory, uint, string memory)
+    {
+        if (_gameStage == 0) {
+            return ("Idle", 0, "");
+        }
+
+        if (_gameStage == 1) {
+            if (block.timestamp <= startStageTime + T1) {
+                return (
+                    "Commit Stage : end in ",
+                    startStageTime + T1 - block.timestamp,
+                    " seconds"
+                );
+            } else if (block.timestamp <= startStageTime + T1 + T2) {
+                return (
+                    "Reveal Stage : end in ",
+                    startStageTime + T1 + T2 - block.timestamp,
+                    " seconds"
+                );
+            } else if (block.timestamp <= startStageTime + T1 + T2 + T3) {
+                return (
+                    "Award Stage : end in ",
+                    startStageTime + T1 + T2 + T3 - block.timestamp,
+                    " seconds"
+                );
+            } else if (block.timestamp > startStageTime + T1 + T2 + T3) {
+                return ("Award Timeout", 0, "");
+            }
+        }
+
+        return ("", 0, "");
     }
 
-    function commitHashedLottery(bytes32 _hashedLottery) public payable {
+    function commitHashedLottery(
+        bytes32 _hashedLottery
+    ) public payable returns (uint256) {
         require(
             msg.value == LotteryFee,
             "require LotteryFee (0.001 ether) to participate"
         );
         require(
-            gameStage == 1,
-            "MultiPartyLottery::commitHashLottery: Game is not in commit stage"
-        );
-        require(
-            num_participants < _N,
+            _num_participants < N,
             "MultiPartyLottery::commitHashLottery: Maximum number of players limit exceeded"
         );
+
+        // start the game when the first player joins
+        if (_gameStage == 0) {
+            _gameStage = 1;
+            startStageTime = block.timestamp;
+        }
+
         require(
-            block.timestamp < startStageTime + _T1,
-            "MultiPartyLottery::commitHashLottery: Timeout"
+            block.timestamp <= startStageTime + T1,
+            "MultiPartyLottery::commitHashLottery: Game is not in commit stage"
         );
 
-        uint256 lotteryId = num_participants;
+        uint256 lotteryId = _num_participants;
 
         lotteries[lotteryId] = Lottery({
             choice: 0,
+            isCommitted: true,
             isRevealed: false,
             isWithdrawn: false,
             addr: msg.sender
@@ -74,20 +109,9 @@ contract MultiPartyLottery is Ownable, CommitReveal {
 
         commit(lotteryId, _hashedLottery);
 
-        num_participants++;
-    }
+        _num_participants++;
 
-    function changeGameStageToReveal() public {
-        require(
-            gameStage == 1,
-            "MultiPartyLottery::changeGameStageToReveal: Game is not in commit stage"
-        );
-        require(
-            block.timestamp > startStageTime + _T1,
-            "MultiPartyLottery::changeGameStageToReveal: Commit stage is not over"
-        );
-        gameStage = 2;
-        startStageTime = block.timestamp;
+        return lotteryId;
     }
 
     function revealLottery(
@@ -96,53 +120,47 @@ contract MultiPartyLottery is Ownable, CommitReveal {
         string memory _salt
     ) public {
         require(
-            gameStage == 2,
+            startStageTime + T1 < block.timestamp &&
+                block.timestamp < startStageTime + T1 + T2,
             "MultiPartyLottery::revealLottery: Game is not in reveal stage"
         );
         require(
-            block.timestamp < startStageTime + _T2,
-            "MultiPartyLottery::revealLottery: Timeout"
+            lotteries[_lotteryId].addr == msg.sender,
+            "MultiPartyLottery::revealLottery: You are not the owner of this lottery"
         );
 
         revealChoice(_lotteryId, _choice, _salt);
 
         lotteries[_lotteryId].choice = _choice;
-        lotteries[_lotteryId].isRevealed;
-    }
-
-    function changeGameStageToAward() public onlyOwner {
-        require(
-            gameStage == 2,
-            "MultiPartyLottery::changeGameStageToAward: Game is not in reveal stage"
-        );
-        require(
-            block.timestamp > startStageTime + _T2,
-            "MultiPartyLottery::changeGameStageToAward: Reveal stage is not over"
-        );
-        gameStage = 3;
-        startStageTime = block.timestamp;
+        lotteries[_lotteryId].isRevealed = true;
+        _num_revealed++;
     }
 
     function awardLottery() public onlyOwner {
         require(
-            gameStage == 3,
+            startStageTime + T1 + T2 < block.timestamp &&
+                block.timestamp < startStageTime + T1 + T2 + T3,
             "MultiPartyLottery::awardLottery: Game is not in award stage"
         );
-        require(
-            block.timestamp < startStageTime + _T3,
-            "MultiPartyLottery::awardLottery: Timeout"
-        );
 
-        Lottery[] memory goodLotteries = new Lottery[](num_participants);
+        Lottery[] memory goodLotteries = new Lottery[](_num_participants);
         uint256 goodLotteriesCount = 0;
 
-        for (uint256 i = 0; i < _N; i++) {
+        for (uint256 i = 0; i < N; i++) {
             if (
                 lotteries[i].isRevealed &&
                 lotteries[i].choice >= MinimumChoice &&
                 lotteries[i].choice <= MaximumChoice
             ) {
-                goodLotteries[goodLotteriesCount] = lotteries[i];
+                goodLotteries[goodLotteriesCount].choice = lotteries[i].choice;
+                goodLotteries[goodLotteriesCount].addr = lotteries[i].addr;
+                goodLotteries[goodLotteriesCount].isCommitted = lotteries[i]
+                    .isCommitted;
+                goodLotteries[goodLotteriesCount].isRevealed = lotteries[i]
+                    .isRevealed;
+                goodLotteries[goodLotteriesCount].isWithdrawn = lotteries[i]
+                    .isWithdrawn;
+
                 goodLotteriesCount++;
             }
         }
@@ -160,42 +178,26 @@ contract MultiPartyLottery is Ownable, CommitReveal {
             goodLotteriesCount;
 
         _rewardWinner(goodLotteries[winnerIndex].addr);
-
-        gameStage = 0;
-        num_participants = 0;
-        startStageTime = block.timestamp;
+        _resetGame();
     }
 
     function _ownerGetAllReward() private {
-        payable(owner()).transfer(LotteryFee * num_participants);
+        payable(owner()).transfer(LotteryFee * _num_participants);
     }
 
     function _rewardWinner(address _winner) private {
-        // winner get 0.001 ETH * num_participants * 0.98
-        // owner get 0.001 ETH * num_participants * 0.02
-        uint256 winnerReward = (LotteryFee * num_participants * 98) / 100;
-        uint256 ownerReward = (LotteryFee * num_participants * 2) / 100;
+        // winner get 0.001 ETH * _num_participants * 0.98
+        // owner get 0.001 ETH * _num_participants * 0.02
+        uint256 winnerReward = (LotteryFee * _num_participants * 98) / 100;
+        uint256 ownerReward = (LotteryFee * _num_participants * 2) / 100;
 
         payable(_winner).transfer(winnerReward);
         payable(owner()).transfer(ownerReward);
     }
 
-    function changeGameStageToAwardTimeout() public {
-        require(
-            gameStage == 3,
-            "MultiPartyLottery::changeGameStageToAwardTimeout: Game is not in award stage"
-        );
-        require(
-            block.timestamp > startStageTime + _T3,
-            "MultiPartyLottery::changeGameStageToAwardTimeout: Award stage is not over"
-        );
-        gameStage = 4;
-        startStageTime = block.timestamp;
-    }
-
     function withdraw(uint256 lotteryId) public {
         require(
-            gameStage == 4,
+            startStageTime + T1 + T2 + T3 < block.timestamp,
             "MultiPartyLottery::withdraw: Game is not in award timeout stage"
         );
         require(
@@ -208,5 +210,18 @@ contract MultiPartyLottery is Ownable, CommitReveal {
         );
         lotteries[lotteryId].isWithdrawn = true;
         payable(msg.sender).transfer(LotteryFee);
+
+        _num_participants--;
+
+        if (_num_participants == 0) {
+            _resetGame();
+        }
+    }
+
+    function _resetGame() private {
+        _gameStage = 0;
+        _num_participants = 0;
+        _num_revealed = 0;
+        startStageTime = block.timestamp;
     }
 }
